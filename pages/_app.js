c307b6e6b34c4c2867c2e53e3f36850d5ab2c1f1
sandesh9988 <sandesh.lastdoor@gmail.com>
2023-05-16 22:50:02 +0545

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import '../styles/index.css'
+import { ApolloProvider } from "@apollo/client/react"
+import { client } from "../lib/apollo"
 
 function MyApp({ Component, pageProps }) {
   return (
-        <Component {...pageProps} />
-    )
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+
+  )
 }
 
 export default MyApp
